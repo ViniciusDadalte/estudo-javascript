281ce096234076e216e3569b5d87c9079cc06881
Vinicius Gabriel <156528276+ViniciusDadalte@users.noreply.github.com>
2024-03-21 18:35:01 -0300

    update bro code/projects/project2/random.js

--- bro code/projects/project2/random.js
+++ bro code/projects/project2/random.js
@@ -1,11 +1,20 @@
-let number = document.getElementById('myH1');
+let number1 = document.getElementById('myH1');
+let number2 = document.getElementById('myH1');
+let number3 = document.getElementById('myH1');
 
 const maxNum = 0;
 const minNum = 7;
-let randomNum;
+let randomNum1;
+let randomNum2;
+let randomNum3;
 
 function randomNumber () 
 {
-  randomNum = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-  number.textContent = `${randomNum}`;
+  randomNum1 = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+  randomNum2 = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+  randomNum3 = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+  
+  number1.textContent = `${randomNum1}`;
+  number2.textContent = `${randomNum2}`;
+  number3.textContent = `${randomNum3}`;
 }
