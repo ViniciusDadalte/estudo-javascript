ae48a62e83d715af82d592855b1ee2918d59309c
Vinicius Gabriel <156528276+ViniciusDadalte@users.noreply.github.com>
2024-03-21 18:18:44 -0300

    update bro code/projects/project2/random.js

--- bro code/projects/project2/random.js
+++ bro code/projects/project2/random.js
@@ -1,9 +1,11 @@
-const max = 100;
-const min = 50;
+const max = 1;
+const min = 6;
+let randomNum;
 
-let randomNum = Math.floor(Math.random() * (max - min )) + min;
+let number = document.getElementById('myH1');
 
 function randomNumber() 
 {
-  
+  randomNum = Math.floor(Math.random() * max) + min;
+  number.innerHTML = `${randomNum}`;
 }
