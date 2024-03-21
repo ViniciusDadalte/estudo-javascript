5d270b82b10614d6a9811510ad199e59d40d9ba9
Vinicius Gabriel <156528276+ViniciusDadalte@users.noreply.github.com>
2024-03-20 23:49:45 -0300

    update bro code/projects/project1/script.js

---
+++ bro code/projects/project1/script.js
@@ -0,0 +1,17 @@
+let content = document.getElementById('contentLabel');
+count = 0;
+
+function Decrease () 
+{
+  content.innerHTML = count - 1;
+}
+
+function Reset () 
+{
+  content.innerHTML = count;
+}
+
+function Increase () 
+{
+  content.innerHTML = count + 1;
+}
