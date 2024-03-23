f87df8571140619800dc5e3f825984ed5ce35f8c
Vinicius Gabriel <156528276+ViniciusDadalte@users.noreply.github.com>
2024-03-23 13:10:48 -0300

    update Coding Classroom/exercises/exercio pontos do time/index.js

---
+++ Coding Classroom/exercises/exercio pontos do time/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const media = (a,b,c) => (a + b + c) / 3;
+
+const scoreEquipe1 = media(44, 23, 71);
+const scoreEquipe2 = media(65, 54, 49);
+
+const ganhador = function (avgEquipe1, avgEquipe2)
+{
+  if (avgEquipe1 => 2 * avgEquipe2)
+  {
+    console.log(`A equipe1 ganhou  ${avgEquipe1} vs. ${avgEquipe2}`);
+  }
+  else if(avgEquipe2 => 2 * avgEquipe1)
+  {
+    console.log(`A equipe2 ganhou  ${avgEquipe2} vs. ${avgEquipe1}`);
+  }
+  else 
+  {
+    console.log(`Nenhum time ganhou...`);
+  }
+}
