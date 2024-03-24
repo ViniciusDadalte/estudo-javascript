d32c3840d13484d798d56a6e8aa59e9ad25c81dc
Vinicius Gabriel <156528276+ViniciusDadalte@users.noreply.github.com>
2024-03-24 17:50:11 -0300

    Contador de idades com functions e arrays

---
+++ Coding Classroom/exercises/exercicio array/script.js
@@ -0,0 +1,11 @@
+//exercicio
+
+const calcIdade = (anoNascento) => 2024 - anoNascento;
+
+const nascimentos = [1990, 1970, 2008];
+
+const idade1 = calcIdade(nascimentos[0]);
+const idade2 = calcIdade(nascimentos[1]);
+const idade3 = calcIdade(nascimentos[2]);
+
+console.log(idade1, idade2, idade3);
