2c7ca087b355b8cbb9027bda75e14e757d6eca7c
Vinicius Gabriel <156528276+ViniciusDadalte@users.noreply.github.com>
2024-03-27 00:52:29 -0300

    update Coding Classroom/exercises/challenge loops/script.js

---
+++ Coding Classroom/exercises/challenge loops/script.js
@@ -0,0 +1,18 @@
+const calcTip = function (bill) 
+{
+  return bill >= 50 && bill <= 300 ? bill * 0.25 : bill * 0.2;
+}
+
+const bills = [25, 225, 176, 440, 46, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++)
+{
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
